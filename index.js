b016b7c820dec4c3efea5f331a3fc2a1b13e8767
Marco Guevara <marcojgh352@gmail.com>
2026-03-05 09:26:41 +0100

    KataFizzBuzz/Final

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 // Kata FizzBuzz
 
-for (let i = 1; i <= 50; i++) {
-  if (i % 3 == 0 && i % 5 !== 0) {
-    console.log("Fizz");
-  }
-  if (i % 5 == 0 && i % 3 !== 0) {
-    console.log("Buzz");
-  }
-  if (i % 3 == 0 && i % 5 == 0) {
-    console.log("FizzBuzz");
-  } else {
-    console.log(i);
+function fizzBuzz() {
+  for (let i = 1; i <= 1000; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 == 0) {
+      console.log("Fizz");
+    } else if (i % 5 == 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
   }
 }
+
+fizzBuzz();
