4c29e5489e6b5f83985d7239faa4fa6645c780da
Marco Guevara <marcojgh352@gmail.com>
2026-03-06 10:58:51 +0100

    Correcciones

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 // Kata FizzBuzz
 
 function fizzBuzz() {
-  for (let i = 1; i <= 1000; i++) {
+  for (let i = 1; i <= 50; i++) {
     if (i % 3 == 0 && i % 5 == 0) {
       console.log("FizzBuzz");
-    } else if (i % 3 == 0) {
-      console.log("Fizz");
-    } else if (i % 5 == 0) {
-      console.log("Buzz");
-    } else {
-      console.log(i);
+      continue;
     }
+    if (i % 3 == 0) {
+      console.log("Fizz");
+      continue;
+    }
+    if (i % 5 == 0) {
+      console.log("Buzz");
+      continue;
+    }
+    console.log(i);
   }
 }
 
